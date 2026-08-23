780606bfdf40eb2a58e2a646b0f07d0df43ce581
KyKay47 <tshabalalaseth17@gmail.com>
2026-08-23 16:16:30 +0200

    Refactor AppModule to provide application context

--- app/src/main/java/com/ferhatozcelik/jetpackcomposetemplate/di/AppModule.kt
+++ app/src/main/java/com/ferhatozcelik/jetpackcomposetemplate/di/AppModule.kt
@@ -1,21 +1,14 @@
 package com.ferhatozcelik.jetpackcomposetemplate.di
 
-import com.ferhatozcelik.jetpackcomposetemplate.data.remote.GoldApiService
-import com.ferhatozcelik.jetpackcomposetemplate.data.repository.GoldRepository
+import android.app.Application
+import android.content.Context
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import javax.inject.Qualifier
 import javax.inject.Singleton
-
-@Retention(AnnotationRetention.RUNTIME)
-@Qualifier
-annotation class ApplicationScope
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -23,27 +16,13 @@ object AppModule {
 
     @Provides
     @Singleton
-    @ApplicationScope
-    fun provideApplicationScope(): CoroutineScope = CoroutineScope(SupervisorJob())
-
-    @Provides
-    @Singleton
-    fun provideRetrofit(): Retrofit {
-        return Retrofit.Builder()
-            .baseUrl("https://api.binance.com/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+    fun provideContext(application: Application): Context {
+        return application.applicationContext
     }
 
     @Provides
     @Singleton
-    fun provideGoldApiService(retrofit: Retrofit): GoldApiService {
-        return retrofit.create(GoldApiService::class.java)
-    }
-
-    @Provides
-    @Singleton
-    fun provideGoldRepository(apiService: GoldApiService): GoldRepository {
-        return GoldRepository(apiService)
+    fun provideApplicationScope(): CoroutineScope {
+        return CoroutineScope(SupervisorJob())
     }
 }
