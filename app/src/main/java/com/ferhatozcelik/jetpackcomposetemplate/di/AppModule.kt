1c06476db7b5a4685f300bd36a56c9d17766f6c4
KyKay47 <tshabalalaseth17@gmail.com>
2026-08-23 18:47:14 +0200

    Update AppModule.kt

--- app/src/main/java/com/ferhatozcelik/jetpackcomposetemplate/di/AppModule.kt
+++ app/src/main/java/com/ferhatozcelik/jetpackcomposetemplate/di/AppModule.kt
@@ -5,8 +5,14 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import javax.inject.Qualifier
 import javax.inject.Singleton
+
+@Retention(AnnotationRetention.RUNTIME)
+@Qualifier
+annotation class ApplicationScope
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -14,7 +20,6 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideApplicationScope(): CoroutineScope {
-        return CoroutineScope(SupervisorJob())
-    }
+    @ApplicationScope
+    fun provideApplicationScope(): CoroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
 }
