5d64912a5b135954529d4d85170f0cba8b1c59d1
KyKay47 <tshabalalaseth17@gmail.com>
2026-08-23 16:51:00 +0200

    Update ExampleRepository.kt

--- app/src/main/java/com/ferhatozcelik/jetpackcomposetemplate/data/repository/ExampleRepository.kt
+++ app/src/main/java/com/ferhatozcelik/jetpackcomposetemplate/data/repository/ExampleRepository.kt
@@ -1,10 +1,17 @@
 package com.ferhatozcelik.jetpackcomposetemplate.data.repository
 
 import com.ferhatozcelik.jetpackcomposetemplate.data.dao.ExampleDao
-import com.ferhatozcelik.jetpackcomposetemplate.data.remote.AppApi
+import com.ferhatozcelik.jetpackcomposetemplate.data.entity.ExampleEntity
 import javax.inject.Inject
-import javax.inject.Singleton
 
+class ExampleRepository @Inject constructor(
+    private val exampleDao: ExampleDao
+) {
+    suspend fun getEntities(): List<ExampleEntity> {
+        return exampleDao.getAll()
+    }
 
-@Singleton
-class ExampleRepository @Inject constructor(private val appApi: AppApi, private val exampleDao: ExampleDao)
+    suspend fun insertEntity(entity: ExampleEntity) {
+        exampleDao.insert(entity)
+    }
+}
