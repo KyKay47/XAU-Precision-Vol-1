69f2274db50b2f561131584b83d2540845faeb22
KyKay47 <tshabalalaseth17@gmail.com>
2026-08-23 07:30:57 +0200

    Update AppApi.kt

--- app/src/main/java/com/ferhatozcelik/jetpackcomposetemplate/data/remote/AppApi.kt
+++ app/src/main/java/com/ferhatozcelik/jetpackcomposetemplate/data/remote/AppApi.kt
@@ -1,12 +1,13 @@
 package com.ferhatozcelik.jetpackcomposetemplate.data.remote
 
-import com.ferhatozcelik.jetpackcomposetemplate.data.model.ExampleModel
-import retrofit2.Response
-import retrofit2.http.*
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface AppApi {
-
-    @GET("/api/v1/example")
-    suspend fun getExampleResult(): Response<List<ExampleModel>>
-
+    @GET("api/v3/klines")
+    suspend fun getKlines(
+        @Query("symbol") symbol: String = "PAXGUSDT",
+        @Query("interval") interval: String,
+        @Query("limit") limit: Int = 75
+    ): List<List<Any>>
 }
