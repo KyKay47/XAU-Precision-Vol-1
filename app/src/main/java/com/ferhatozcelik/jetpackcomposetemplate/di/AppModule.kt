e4d7163ecd1cb7754aacf9ee910f11905ae8b746
KyKay47 <tshabalalaseth17@gmail.com>
2026-08-23 10:57:01 +0200

    Introduce CoroutineScope with ApplicationScope qualifier
    Added ApplicationScope qualifier and CoroutineScope provider.

--- app/src/main/java/com/ferhatozcelik/jetpackcomposetemplate/di/AppModule.kt
+++ app/src/main/java/com/ferhatozcelik/jetpackcomposetemplate/di/AppModule.kt
@@ -1,18 +1,28 @@
 package com.ferhatozcelik.jetpackcomposetemplate.di
 
-import com.ferhatozcelik.jetpackcomposetemplate.data.remote.GoldApiService
-import com.ferhatozcelik.jetpackcomposetemplate.data.repository.GoldRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Qualifier
 import javax.inject.Singleton
+
+@Retention(AnnotationRetention.RUNTIME)
+@Qualifier
+annotation class ApplicationScope
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    @Singleton
+    @ApplicationScope
+    fun provideApplicationScope(): CoroutineScope = CoroutineScope(SupervisorJob())
 
     @Provides
     @Singleton
@@ -21,17 +31,5 @@ object AppModule {
             .baseUrl("https://api.binance.com/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-    }
-
-    @Provides
-    @Singleton
-    fun provideGoldApiService(retrofit: Retrofit): GoldApiService {
-        return retrofit.create(GoldApiService::class.java)
-    }
-
-    @Provides
-    @Singleton
-    fun provideGoldRepository(apiService: GoldApiService): GoldRepository {
-        return GoldRepository(apiService)
     }
 }
