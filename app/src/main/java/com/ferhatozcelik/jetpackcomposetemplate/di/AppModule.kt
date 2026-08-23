1140a7fc76341e0f28d52d5724dca388545a8ebd
KyKay47 <tshabalalaseth17@gmail.com>
2026-08-23 19:50:47 +0200

    Update AppModule.kt

--- app/src/main/java/com/ferhatozcelik/jetpackcomposetemplate/di/AppModule.kt
+++ app/src/main/java/com/ferhatozcelik/jetpackcomposetemplate/di/AppModule.kt
@@ -1,35 +1,21 @@
 package com.ferhatozcelik.jetpackcomposetemplate.di
 
-import android.content.Context
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
-import javax.inject.Qualifier
 import javax.inject.Singleton
-
-@Retention(AnnotationRetention.RUNTIME)
-@Qualifier
-annotation class ApplicationScope
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
-    @ApplicationScope
     @Provides
     @Singleton
     fun provideApplicationScope(): CoroutineScope {
         return CoroutineScope(SupervisorJob() + Dispatchers.Default)
-    }
-
-    @Provides
-    @Singleton
-    fun provideContext(@ApplicationContext context: Context): Context {
-        return context
     }
 }
