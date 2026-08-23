190850c8b3ee7fb21af33c08a730e5e5b8e952ee
KyKay47 <tshabalalaseth17@gmail.com>
2026-08-23 08:07:16 +0200

    Update AppModule.kt

--- app/src/main/java/com/ferhatozcelik/jetpackcomposetemplate/di/AppModule.kt
+++ app/src/main/java/com/ferhatozcelik/jetpackcomposetemplate/di/AppModule.kt
@@ -1,27 +1,29 @@
 package com.ferhatozcelik.jetpackcomposetemplate.di
 
+import com.ferhatozcelik.jetpackcomposetemplate.data.remote.GoldApiService
+import com.ferhatozcelik.jetpackcomposetemplate.data.repository.GoldRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.SupervisorJob
-import javax.inject.Qualifier
+import retrofit2.Retrofit
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
-    @ApplicationScope
+    // ... Keep existing providers like provideRetrofit here ...
+
     @Provides
     @Singleton
-    fun provideApplicationScope(): CoroutineScope {
-        return CoroutineScope(SupervisorJob())
+    fun provideGoldApiService(retrofit: Retrofit): GoldApiService {
+        return retrofit.create(GoldApiService::class.java)
     }
 
+    @Provides
+    @Singleton
+    fun provideGoldRepository(apiService: GoldApiService): GoldRepository {
+        return GoldRepository(apiService)
+    }
 }
-
-@Retention(AnnotationRetention.RUNTIME)
-@Qualifier
-annotation class ApplicationScope
