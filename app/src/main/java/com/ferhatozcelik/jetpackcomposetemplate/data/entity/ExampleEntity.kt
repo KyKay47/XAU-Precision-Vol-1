14ed27221f196916ab6de97d9fb5d2a2b4156e3b
KyKay47 <tshabalalaseth17@gmail.com>
2026-08-23 16:25:37 +0200

    Update ExampleEntity.kt

--- app/src/main/java/com/ferhatozcelik/jetpackcomposetemplate/data/entity/ExampleEntity.kt
+++ app/src/main/java/com/ferhatozcelik/jetpackcomposetemplate/data/entity/ExampleEntity.kt
@@ -1,17 +1,11 @@
 package com.ferhatozcelik.jetpackcomposetemplate.data.entity
 
-import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import kotlinx.parcelize.Parcelize
 
-@Parcelize
 @Entity(tableName = "example_table")
 data class ExampleEntity(
     @PrimaryKey(autoGenerate = true)
-    var id: Int? = null,
-    val title: String?,
-    val description: String?
-) : Parcelable
-
-
+    val id: Int = 0,
+    val name: String
+)
