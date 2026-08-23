ec934012b9310e8dfe7d18c6e25448f106e4498f
KyKay47 <tshabalalaseth17@gmail.com>
2026-08-23 14:43:41 +0200

    Update AppApi.kt

--- app/src/main/java/com/ferhatozcelik/jetpackcomposetemplate/data/remote/AppApi.kt
+++ app/src/main/java/com/ferhatozcelik/jetpackcomposetemplate/data/remote/AppApi.kt
@@ -3,11 +3,9 @@ package com.ferhatozcelik.jetpackcomposetemplate.data.remote
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-interface AppApi {
-    @GET("api/v3/klines")
-    suspend fun getKlines(
-        @Query("symbol") symbol: String = "PAXGUSDT",
-        @Query("interval") interval: String,
-        @Query("limit") limit: Int = 75
-    ): List<List<Any>>
+interface GoldApiService {
+    @GET("api/v3/ticker/price")
+    suspend fun getSymbolPrice(
+        @Query("symbol") symbol: String = "PAXGUSDT"
+    ): Any
 }
