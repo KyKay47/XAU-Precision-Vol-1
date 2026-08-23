d8c699e4cea7a7a70bf59a6b18f6dc7c52d76b62
KyKay47 <tshabalalaseth17@gmail.com>
2026-08-23 16:30:07 +0200

    Update AppDatabase to disable exportSchema

--- app/src/main/java/com/ferhatozcelik/jetpackcomposetemplate/data/local/AppDatabase.kt
+++ app/src/main/java/com/ferhatozcelik/jetpackcomposetemplate/data/local/AppDatabase.kt
@@ -2,22 +2,10 @@ package com.ferhatozcelik.jetpackcomposetemplate.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
 import com.ferhatozcelik.jetpackcomposetemplate.data.dao.ExampleDao
 import com.ferhatozcelik.jetpackcomposetemplate.data.entity.ExampleEntity
-import com.ferhatozcelik.jetpackcomposetemplate.di.ApplicationScope
-import kotlinx.coroutines.CoroutineScope
-import javax.inject.Inject
-import javax.inject.Provider
 
-@Database(entities = [ExampleEntity::class], version = 1)
-@TypeConverters(Converters::class)
+@Database(entities = [ExampleEntity::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
-
-    abstract fun getExampleDao(): ExampleDao
-
-    class Callback @Inject constructor(
-        private val database: Provider<AppDatabase>,
-        @ApplicationScope private val applicationScope: CoroutineScope
-    ) : RoomDatabase.Callback()
+    abstract fun exampleDao(): ExampleDao
 }
