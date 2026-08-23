b690a09b738a331c2aeb72da396f83e741722f9c
KyKay47 <tshabalalaseth17@gmail.com>
2026-08-23 21:33:10 +0200

    Update AppApi.kt

--- app/src/main/java/com/ferhatozcelik/jetpackcomposetemplate/data/remote/AppApi.kt
+++ app/src/main/java/com/ferhatozcelik/jetpackcomposetemplate/data/remote/AppApi.kt
@@ -6,7 +6,7 @@ import retrofit2.http.Query
 interface GoldApiService {
     @GET("api/v3/klines")
     suspend fun getKlines(
-        @Query("symbol") symbol: String = "XAUUSDT",
+        @Query("symbol") symbol: String = "PAXGUSDT", // Set to Gold spot symbol
         @Query("interval") interval: String,
         @Query("limit") limit: Int = 100
     ): List<List<Any>>
